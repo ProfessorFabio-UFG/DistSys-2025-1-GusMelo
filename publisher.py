57f3f97ef828804c0f5080dd2cc2efbc554d7ca5
GusMelo <107869105+GusMelo@users.noreply.github.com>
2025-05-18 22:33:10 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,11 +1,26 @@
-import zmq, time
-from constPS import * #-
+import zmq
+import time
+from datetime import datetime
+from constPS import *  # -
 
 context = zmq.Context()
 s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp://"+HOST+":"+ PORT      # how and where to communicate
+p = "tcp://" + HOST + ":" + PORT   # how and where to communicate
 s.bind(p)                          # bind socket to the address
+
 while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+    time.sleep(5)  # wait every 5 seconds
+
+    # Tópico TIME
+    msg_time = str.encode("TIME " + time.asctime())
+    s.send(msg_time)
+
+    # Tópico DATE
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    msg_date = str.encode("DATE " + current_date)
+    s.send(msg_date)
+
+    # Tópico STATUS
+    msg_status = str.encode("STATUS OK")
+    s.send(msg_status)
+
